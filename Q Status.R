1f7fcc44439314c7329597f9e6f6d64beaa59e02
yaoyunhz <yaoyunhz@gmail.com>
2019-09-11 08:48:11 -0600

    Update Q Status.R

--- Q Status.R
+++ Q Status.R
@@ -9,19 +9,20 @@ library(Hmisc)
 
 # Load Data ---------------------------------------------------------------
 
-master_schools <- read.csv("MasterSchoolsAllYearsCorrected2018V7.csv", 
+# master schools
+master_schools <- read.csv("Master Schools 2019 V4.csv", 
                            header = TRUE, stringsAsFactors = FALSE)
 
+# prior1 data
 avt18_raw <- read.csv("All Valid Tests 2018V5.csv", 
                   header = TRUE, stringsAsFactors = FALSE)
 
 schools2018 <- master_schools[master_schools$ï..SY == 2018, ]
+nrow(schools2018) # N = 1788
 
 
-avt17_raw <- read.csv("All Valid Tests 2017 V10.csv",
-                      header = TRUE, stringsAsFactors = FALSE)
+# current year data
 
-schools2017 <- master_schools[master_schools$ï..SY == 2017, ]
    
 
 
@@ -60,22 +61,6 @@ Q_status <- function(raw_data) {
 }
 
 
-# # define function "counts_by_Q"
-# counts_by_Q <- function(data_file, school_file) {
-#     data_file %>%
-#         group_by(schnumb) %>%
-#         count(Qstatus) %>%
-#         spread(key = Qstatus, value = n) %>%
-#         mutate(total_students = Q1 + Q23 + Q4,
-#                percent_Q1 = Q1 / total_students,
-#                percent_Q23 = Q23 / total_students,
-#                percent_Q4 = Q4 / total_students) %>%
-#         left_join(school_file, by = "schnumb") %>%
-#         select(schnumb, AGAID, distname, schname, Q1, Q23, Q4,
-#                total_students, percent_Q1, percent_Q23, percent_Q4)
-#     }
-
-
 
 # Prior1 Year Q Status ----------------------------------------------------
 
@@ -98,38 +83,7 @@ current_date <- Sys.Date()
 file_name <- paste0("Prior1 Q Status (2017-2018) ", current_date, ".csv")
 write.csv(avt18_Q, file = file_name, row.names = FALSE)
 
-# counts18 <- counts_by_Q(avt18_Q, schools2018)
-# counts18
-
-# # save output
-# current_date <- Sys.Date()
-# file_name <- paste0("Counts 2018 by Q Status by School ", current_date, ".csv")
-# write.csv(avt18, file = file_name, row.names = FALSE)
 
 
+# Current Year Q Status ---------------------------------------------------
 
-# Prior2 Year Q Status ----------------------------------------------------
-
-# school year 2016-2017
-avt17_Q <- Q_status(raw_data = avt17_raw)
-avt17_Q$SY <- 2017
-avt17_Q
-
-# check for duplicates by subject
-avt17_Q %>%
-    filter(Subtest == "READ") %>%
-    group_by(StID, Subtest) %>%
-    filter(n() > 1)
-
-
-# save output
-current_date <- Sys.Date()
-file_name <- paste0("Prior2 Q Status (2016-2017) ", current_date, ".csv")
-write.csv(avt17_Q, file = file_name, row.names = FALSE)
-
-
-
-
-# Select the Earliest Possible Q Status -----------------------------------
-combined <- rbind(avt17_Q, avt18_Q)
-combined[combined$StID == 103342069, ]
